29b1107fe902f7aeaf2cfe51e7912445a0b330f1
shivam491 <shivam4918@gmail.com>
2025-12-01 19:01:49 +0530

    chore: add ASGI routing placeholder

---
+++ medqueue/routing.py
@@ -0,0 +1,9 @@
+from django.urls import re_path
+
+# import consumers here when you create them
+# from api import consumers
+
+websocket_urlpatterns = [
+    # Example placeholder (no consumer yet)
+    # re_path(r'ws/queue/(?P<doctor_id>\d+)/$', consumers.QueueConsumer.as_asgi()),
+]
